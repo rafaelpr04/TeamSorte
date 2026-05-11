fdaa87f836481272a72157d6909347d486251db4
Djalmirb <djalmirb@gmail.com>
2026-05-11 19:24:10 -0300

    Acrescentei um atributo

--- lib/models/jogador.dart
+++ lib/models/jogador.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 
 class Jogador {
+  final String id;
   final String nome;
   final int nivel;
 
-  Jogador({required this.nome, required this.nivel});
+  Jogador({required this.id, required this.nome, required this.nivel});
 }
